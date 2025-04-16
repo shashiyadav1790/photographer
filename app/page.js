5c268265efee5e8f7b08172384736041b87982f4
shashi yadav <shashiyadav1790@gmail.com>
2025-04-17 00:57:13 +0530

    some changes

--- app/page.js
+++ app/page.js
@@ -5,8 +5,7 @@ import About from "./about/page.js";
 import Experience from "./expirence/page.js";
 import Service from "./service/page.js";
 import Contact from "./contact/page.js";
-import Footer from "./footer/page.js";
-import SignUp from "./signup.js";
+
 function Page() {
 
 
